c4e23d29c03adbee3f67f515ede96a7b68799f5f
CalebKangebeniMatias <calebkangebenimatias@gmail.com>
2026-03-01 01:05:26 +0100

    Site oficial de expo4

--- gestao_autocarros/settings.py
+++ gestao_autocarros/settings.py
@@ -60,7 +60,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # 🔹 Autenticação
